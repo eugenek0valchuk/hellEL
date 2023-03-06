3006c77fe1e20bc8b701337e3f1447629366a567
eugenek0valchuk <eugenekovalchuki13@gmail.com>
2023-03-06 23:12:14 +0300

    EL-004

--- hellel/src/app/navigation/main.jsx
+++ hellel/src/app/navigation/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+// import { Footer } from '../../shared/components/footer'
+import { Header } from '../../shared/components/header'
 
 export const Main = () => {
   return (
-    <div>startPageAfterLogin</div>
+    <>
+      <Header />
+      {/* <Footer /> */}
+    </>
   )
 }
